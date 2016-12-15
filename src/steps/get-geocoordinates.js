482aab4c35e69b67e937da69e0239d9d7cefb5bf
Manuel Serra <nitroin@hotmail.com>
2016-12-15 12:19:02 +0100

    Add debug logs

--- src/steps/get-geocoordinates.js
+++ src/steps/get-geocoordinates.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 import get from "lodash.get";
+import url from "url";
 
 import {GOOGLE_GEO_API_URL} from "../config";
 import log from "../services/logger";
 
 export async function getGeocoordinates(address) {
-    const result = await axios.get(`${GOOGLE_GEO_API_URL}/maps/api/geocode/json?address=${address}`);
+    const request = url.resolve(GOOGLE_GEO_API_URL, `/maps/api/geocode/json?address=${address}`);
+    const result = await axios.get(request);
     log.debug({
+        request,
         response: result.data
     });
     if (result && result.data && result.data.status === "OK" && result.data.results[0]) {
